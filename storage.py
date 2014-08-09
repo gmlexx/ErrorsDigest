671c86479417cb9c55fc451c11368e56d2a29711
Alexey Larkov <gmlexx@gmail.com>
2014-08-09 14:29:04 +0600

    Add pattern rank calculation

--- storage.py
+++ storage.py
@@ -1,108 +1,102 @@
-import os, codecs, sys
+import os, codecs
 from datetime import datetime, timedelta
 from tree import TPattern, Tree
+from progressbar import ProgressBar, Bar, Percentage, FileTransferSpeed
 
 DATA_PATH = os.path.realpath('./data')
 TREE = Tree()
 
-def get_hash(str):
-	return hash(str) & sys.maxint
-
 def load_patterns():
-	loaded_patterns_hashes = {}
-	pattern_path = os.path.join(DATA_PATH, "patterns")
-	if os.path.exists(pattern_path):
-		f = codecs.open(pattern_path, mode="r", encoding='utf-8')
-		count = 0
-		for line in f:
-			if line.strip() != "":
-				if count % 3 == 0:
-					name = line.strip()
-				if count % 3 == 1:
-					logger = line.strip()
-				if count % 3 == 2:
-					pattern = line.strip()
-					hash = get_hash("".join([name, logger, pattern]))
-					if hash not in TREE.patterns:
-						TREE.patterns.update({hash: TPattern(name, logger, pattern)})
-						TREE.patterns_order.insert(0, hash)
-					loaded_patterns_hashes.update({hash:name})
-			count += 1
-		f.close()
-	if len(TREE.patterns) == 0:
-		hash = get_hash("Default")
-		TREE.patterns.update({hash: TPattern("Default", ".*", ".*")})
-		TREE.patterns_order.insert(0, hash)
-	for pattern_hash in TREE.patterns.keys():
-		if pattern_hash not in loaded_patterns_hashes:
-			latest_data = [item for item in TREE.patterns[pattern_hash].latest_data]
-			del TREE.patterns[pattern_hash] 
-			TREE.patterns_order.remove(pattern_hash)
-			for item in latest_data:
-				TREE.process_data_dict(item)
+    loaded_patterns_hashes = {}
+    pattern_path = os.path.join(DATA_PATH, "patterns")
+    if os.path.exists(pattern_path):
+        with codecs.open(pattern_path, mode="r", encoding='utf-8') as f:
+            count = 0
+            for line in f:
+                if line.strip() != "":
+                    if count % 3 == 0:
+                        name = line.strip()
+                    if count % 3 == 1:
+                        logger = line.strip()
+                    if count % 3 == 2:
+                        pattern = line.strip()
+                        hash_value = TREE.add_pattern(TPattern(name, logger, pattern))
+                        loaded_patterns_hashes.update({hash_value:None})
+                count += 1
+    TREE.add_pattern(TPattern("Default", ".*", ".*", default = True))
+    for pattern_hash in TREE.patterns.keys():
+        if pattern_hash not in loaded_patterns_hashes:
+            latest_data = [item for item in TREE.patterns[pattern_hash].latest_data]
+            del TREE.patterns[pattern_hash]
+            TREE.patterns_order.remove(pattern_hash)
+            for item in latest_data:
+                TREE.process_data_dict(item)
 
 def load():
 
-	load_patterns()
-	raw_datapath = os.path.join(DATA_PATH, "raw")
+    load_patterns()
+    raw_datapath = os.path.join(DATA_PATH, "raw")
 
-	if os.path.exists(raw_datapath):
-		data_files = os.listdir(raw_datapath)
-		data_files.sort()
-		for file in data_files:
-			file_date = datetime.strptime(file, "%Y-%m-%d")
-			if timedelta(days=2) + file_date < datetime.now():
-				continue
-			f = codecs.open(os.path.join(raw_datapath, file), mode="r", encoding='utf-8')
-			data = ""
-			for line in f:
-				if line[:5] == "ERROR" or line[:5] == "INFO " or line[:5] == "WARN " or line[:5] == "FATAL":
-					TREE.put(data)
-					data = ""
-				data += line
-			TREE.put(data)
-			f.close()
-	else:
-		os.makedirs(raw_datapath)
+    if os.path.exists(raw_datapath):
+        data_files = os.listdir(raw_datapath)
+        data_files.sort()
+        for file in data_files:
+            file_date = datetime.strptime(file, "%Y-%m-%d")
+            if timedelta(days=2) + file_date < datetime.now():
+                continue
+            filename = os.path.join(raw_datapath, file)
+            with codecs.open(filename, mode="r", encoding='utf-8') as f:
+                data = ""
+                total_size = os.path.getsize(filename)
+                if total_size == 0:
+                    continue
+                pbar = ProgressBar(widgets=['Loading %s: ' % file, Percentage(), Bar(), FileTransferSpeed()], maxval=total_size).start()
+                for line in f:
+                    if line[:5] == "ERROR" or line[:5] == "INFO " or line[:5] == "WARN " or line[:5] == "FATAL":
+                        TREE.put(data)
+                        data = ""
+                    data += line
+                    pbar.update(f.tell())
+                pbar.finish()
+                TREE.put(data)
+    else:
+        os.makedirs(raw_datapath)
 
 def reload_latest_data():
-	load_patterns()
-	defaultPattern = TREE.patterns[TREE.patterns_order[-1]]
-	latest_data = [item for item in defaultPattern.latest_data]
-	defaultPattern.latest_data.clear()
-	for item in latest_data:
-		defaultPattern.hosts[item['host']].remove(item)
-		TREE.process_data_dict(item)
+    load_patterns()
+    defaultPattern = TREE.patterns[TREE.patterns_order[-1]]
+    latest_data = [item for item in defaultPattern.latest_data]
+    defaultPattern.latest_data.clear()
+    for item in latest_data:
+        defaultPattern.hosts[item['host']].remove(item)
+        TREE.process_data_dict(item)
 
 def put(data):
-	date = TREE.put(data)
-	date_file_path = os.path.join(os.path.join(DATA_PATH, "raw", date))
-	f = codecs.open(date_file_path, mode="a", encoding='utf-8')
-	f.write("%s\n" % data)
-	f.close()
+    date = TREE.put(data)
+    date_file_path = os.path.join(os.path.join(DATA_PATH, "raw", date))
+    f = codecs.open(date_file_path, mode="a", encoding='utf-8')
+    f.write("%s\n" % data)
+    f.close()
 
 def get_raw_patterns():
-	pattern_path = os.path.join(DATA_PATH, "patterns")
-	if os.path.exists(pattern_path):
-		f = codecs.open(pattern_path, mode="r", encoding='utf-8')
-		data = f.read()
-		f.close()
-		return data.encode('utf-8')
-	return ""
+    pattern_path = os.path.join(DATA_PATH, "patterns")
+    if os.path.exists(pattern_path):
+        with codecs.open(pattern_path, mode="r", encoding='utf-8') as f:
+            data = f.read()
+            return data.encode('utf-8')
+    return ""
 
 def save_patterns(data):
-	pattern_path = os.path.join(DATA_PATH, "patterns")
-	f = codecs.open(pattern_path, mode="w", encoding='utf-8')
-	f.write(data.rstrip('\n').decode('utf8'))
-	f.write(u'\n')
-	f.close()
-	reload_latest_data()
+    pattern_path = os.path.join(DATA_PATH, "patterns")
+    with codecs.open(pattern_path, mode="w", encoding='utf-8') as f:
+        f.write(data.rstrip('\n').decode('utf8'))
+        f.write(u'\n')
+    reload_latest_data()
 
 def add_pattern(name, logger, pattern):
-        pattern_path = os.path.join(DATA_PATH, "patterns")
-        f = codecs.open(pattern_path, mode="a", encoding='utf-8')
+    pattern_path = os.path.join(DATA_PATH, "patterns")
+    with codecs.open(pattern_path, mode="a", encoding='utf-8') as f:
         f.write("# %s\n" % name)
         f.write("%s\n" % logger)
         f.write("%s\n" % pattern)
-        f.close()
-        reload_latest_data()
+    reload_latest_data()
--- tree.py
+++ tree.py
@@ -1,187 +1,218 @@
-import re, uuid
+import re, uuid, sys
 from collections import deque
 from datetime import datetime, timedelta
 
 space_characters = [' ','\t','\n','\r']
 
+
 class THost:
 
-	def __init__(self, name):
-		self.name = name
-		self.errors = {}
-		self.latest_data = deque()
+    def __init__(self, name):
+        self.name = name
+        self.errors = {}
+        self.latest_data = deque()
 
-	def put(self, data_dict):
-		find = False
-		ts = data_dict['ts']
-		data_dict['message_lines'] = data_dict['message'].split('\r\n')
-		for timestamp in self.errors.keys():
-			if (ts - timestamp).seconds < 60:
-				self.errors[timestamp] += 1
-				find = True
-			if timestamp < datetime.now() - timedelta(hours=48):
-				del self.errors[timestamp]
-		if find == False:
-			self.errors.update({data_dict['ts']: 1})
-		self.latest_data.append(data_dict)
-		if len(self.latest_data) > 10:
-			self.latest_data.popleft()
+    def put(self, data_dict):
+        find = False
+        ts = data_dict['ts']
+        data_dict['message_lines'] = data_dict['message'].split('\r\n')
+        for timestamp in self.errors.keys():
+            if (ts - timestamp).seconds < 60:
+                self.errors[timestamp] += 1
+                find = True
+            if timestamp < datetime.now() - timedelta(hours=48):
+                del self.errors[timestamp]
+        if find == False:
+            self.errors.update({data_dict['ts']: 1})
+        self.latest_data.append(data_dict)
+        if len(self.latest_data) > 10:
+            self.latest_data.popleft()
 
-	def remove(self, data_dict):
-		ts = data_dict['ts']
-		items_to_remove = []
-		for item in self.latest_data:
-			if item['id'] == data_dict['id']:
-				items_to_remove.append(item)
-				for timestamp in self.errors.keys():
-					if (ts - timestamp).seconds < 60:
-						self.errors[timestamp] -= 1
-		for item in items_to_remove:
-			self.latest_data.remove(item)
+    def remove(self, data_dict):
+        ts = data_dict['ts']
+        items_to_remove = []
+        for item in self.latest_data:
+            if item['id'] == data_dict['id']:
+                items_to_remove.append(item)
+                for timestamp in self.errors.keys():
+                    if (ts - timestamp).seconds < 60:
+                        self.errors[timestamp] -= 1
+        for item in items_to_remove:
+            self.latest_data.remove(item)
 
-	def get_count(self, td):
-		count = 0
-		for ts in self.errors.keys():
-			if ts > td:
-				count += self.errors[ts]
-		return count
+    def get_count(self, td):
+        count = 0
+        for ts in self.errors.keys():
+            if ts > td:
+                count += self.errors[ts]
+        return count
 
 class TPattern:
 
-	def __init__(self, name, logger, pattern):
-		self.logger = logger
-		self.pattern = pattern
-		self.name = name
-		self.logger_re = re.compile(logger, re.DOTALL)
-		self.pattern_re = re.compile(pattern, re.DOTALL)
-		self.hosts = {}
-		self.latest_data = deque()
-		self.last_ts = None
+    def __init__(self, name, logger, pattern, default = False):
+        self.logger = logger
+        self.pattern = pattern
+        self.name = name
+        self.logger_re = re.compile(logger, re.DOTALL)
+        self.pattern_re = re.compile(pattern, re.DOTALL)
+        self.hosts = {}
+        self.latest_data = deque()
+        self.last_ts = None
+        self.default = default
 
-	def put(self, data_dict):
-		logger = data_dict['logger']
-		message = data_dict['message']
-		host = data_dict['host']
-		if self.logger_re.match(logger) != None:
-			if self.pattern_re.match(message) != None:
-				self.latest_data.append(data_dict)
-				if len(self.latest_data) > 10: 
-					self.latest_data.popleft()
-				if host not in self.hosts:
-					self.hosts.update({host: THost(host)})
-				self.hosts[host].put(data_dict)
-				self.last_ts = data_dict['ts']
-				return True
-		return False
+    def put(self, data_dict):
+        logger = data_dict['logger']
+        message = data_dict['message']
+        host = data_dict['host']
+        if self.default or self.logger_re.match(logger) != None:
+            if self.default or self.pattern_re.match(message) != None:
+                self.latest_data.append(data_dict)
+                if len(self.latest_data) > 10:
+                    self.latest_data.popleft()
+                if host not in self.hosts:
+                    self.hosts.update({host: THost(host)})
+                self.hosts[host].put(data_dict)
+                self.last_ts = data_dict['ts']
+                return True
+        return False
 
-	def get_data(self, host, td):
-		if host not in self.hosts:
-			return []
-		data = [ item for item in self.hosts[host].latest_data if item['ts'] > td ]
-		data.reverse()
-		return data
+    def get_data(self, host, td):
+        if host not in self.hosts:
+            return []
+        data = [ item for item in self.hosts[host].latest_data if item['ts'] > td ]
+        data.reverse()
+        return data
 
-	def get_counts(self, time_deltas):
-		counts = []
-		total = 0
-		for min in time_deltas:
-			td = datetime.now() - timedelta(minutes=min)
-			count = 0
-			for host in self.hosts.keys():
-				count += self.hosts[host].get_count(td)
-				total += count
-			counts.append({'min': min, 'count': count})
-		return (counts, total)
+    def get_counts(self, time_deltas):
+        counts = []
+        total = 0
+        for min in time_deltas:
+            td = datetime.now() - timedelta(minutes=min)
+            count = 0
+            for host in self.hosts.keys():
+                count += self.hosts[host].get_count(td)
+                total += count
+            counts.append({'min': min, 'count': count})
+        return (counts, total)
 
-	def get_details(self, td):
-		details = {'name': self.name, 'count': 0, 'hosts': []}
-		for host in self.hosts.keys():
-			host_count = self.hosts[host].get_count(td)
-			if host_count > 0:
-				details['hosts'].append({'name': host, 'count': self.hosts[host].get_count(td)})
-				details['count'] += host_count
-		return details
+    def get_details(self, td):
+        details = {'name': self.name, 'count': 0, 'hosts': []}
+        for host in self.hosts.keys():
+            host_count = self.hosts[host].get_count(td)
+            if host_count > 0:
+                details['hosts'].append({'name': host, 'count': self.hosts[host].get_count(td)})
+                details['count'] += host_count
+        return details
 
-	def get_last_ts_delta(self, now):
-		if not self.last_ts:
-			return ""
-		delta = timedelta(0) if now < self.last_ts else now - self.last_ts
-		str_result = "%s days %s hours %s min" % (delta.days, int(delta.seconds/3600), int(delta.seconds%3600/60))
-		return str_result.replace('0 days ','').replace('0 hours ','')
+    def get_last_ts_delta(self, now):
+        if not self.last_ts:
+            return ""
+        delta = timedelta(0) if now < self.last_ts else now - self.last_ts
+        str_result = "%s days %s hours %s min" % (delta.days, int(delta.seconds/3600), int(delta.seconds%3600/60))
+        return str_result.replace('0 days ','').replace('0 hours ','')
 
 class Tree:
 
-	def __init__(self):
-		self.patterns = {}
-		self.patterns_order = []
+    def __init__(self):
+        self.patterns = {}
+        self.patterns_order = []
+        self.patterns_rank = {}
+        self.rank_counter = 0
+        self.ranked_patterns = None
+        self.default_pattern = None
 
-	def clear(self):
-		self.patterns = {}
-		self.patterns_order = []
+    def add_pattern(self, pattern):
+        hash_value = hash("".join([pattern.name, pattern.logger, pattern.pattern])) & sys.maxint
+        self.patterns.update({hash_value: pattern})
+        if not pattern.default:
+            self.patterns_rank.update({hash_value: 0})
+            self.patterns_order.insert(0, hash_value)
+        else:
+            self.patterns_order.append(hash_value)
+            self.default_pattern = pattern
+        return hash_value
 
-	def put(self, data):
-		parts = []
-		word = []
-		meet_square_bracket = False
-		parts_count = 0
-		c_count = 0
-		for c in data:
-			c_count +=1
-			if c == '[':
-				meet_square_bracket = True
-			if c == ']':
-				meet_square_bracket = False
-			if c in space_characters and not meet_square_bracket:
-				parts.append(''.join(word))
-				word = []
-				parts_count += 1
-				if parts_count == 6:
-					break
-			else:
-				word.append(c)
+    def get_ranked_patterns(self):
+        if not self.ranked_patterns:
+            self.ranked_patterns = sorted(self.patterns_rank, key=self.patterns_rank.get, reverse = True)
+            self.rank_counter = 0
+        return self.ranked_patterns
 
-		parts.append(data[c_count:])
+    def clear(self):
+        self.patterns = {}
+        self.patterns_order = []
+        self.rank_counter = 0
+        self.ranked_patterns = None
 
-		if len(parts) > 5:
-			level = parts[0]
-			host = parts[4]
-			logger = parts[5]
-			message = " ".join((parts[6:]))
-			ts = datetime.strptime("%s %s" % (parts[1], parts[2]), "%Y-%m-%d %H:%M:%S,%f")
-			ts_string = "".join([parts[1], " ", parts[2]])
-			data_dict = {'level':level, 'host':host, 'logger':logger, 'message':message, 'ts':ts, 'id': uuid.uuid4(), 'ts_string':ts_string}
-			self.process_data_dict(data_dict)
+    def put(self, data):
+        parts = []
+        word = []
+        meet_square_bracket = False
+        parts_count = 0
+        c_count = 0
+        for c in data:
+            c_count +=1
+            if c == '[':
+                meet_square_bracket = True
+            if c == ']':
+                meet_square_bracket = False
+            if c in space_characters and not meet_square_bracket:
+                parts.append(''.join(word))
+                word = []
+                parts_count += 1
+                if parts_count == 6:
+                    break
+            else:
+                word.append(c)
 
-		return datetime.now().date().strftime("%Y-%m-%d")
+        parts.append(data[c_count:])
 
-	def process_data_dict(self, data_dict):
-		for hash_value in self.patterns_order:
-			if self.patterns[hash_value].put(data_dict):
-				return
+        if len(parts) > 5:
+            level = parts[0]
+            host = parts[4]
+            logger = parts[5]
+            message = " ".join((parts[6:]))
+            ts = datetime.strptime("%s %s" % (parts[1], parts[2]), "%Y-%m-%d %H:%M:%S,%f")
+            ts_string = "".join([parts[1], " ", parts[2]])
+            data_dict = {'level':level, 'host':host, 'logger':logger, 'message':message, 'ts':ts, 'id': uuid.uuid4(), 'ts_string':ts_string}
+            self.process_data_dict(data_dict)
 
-	def get_digest(self, time_deltas):
-		digest = []
-		now = datetime.now()
-		for hash in self.patterns_order:
-			pattern = self.patterns[hash]
-			counts, total = pattern.get_counts(time_deltas)
-			digest.append({'name': pattern.name, 'total': total, 'counts': counts, 'hash': hash, 'is_default': (pattern.name.lower().replace(' ','') == '#default'), 'last_ts_delta': pattern.get_last_ts_delta(now)})
-		return {'digest':digest}
+        return datetime.now().date().strftime("%Y-%m-%d")
 
-	def get_host_data(self, hash, host, td):
-		return {'data': self.patterns[hash].get_data(host, td)}
+    def process_data_dict(self, data_dict):
+        for hash_value in self.get_ranked_patterns():
+            pattern = self.patterns[hash_value]
+            if pattern.put(data_dict):
+                self.patterns_rank[hash_value] += 1
+                self.rank_counter += 1
+                if self.rank_counter > 1000:
+                    self.ranked_patterns = None
+                return
+        self.default_pattern.put(data_dict)
 
-	def get_details(self, hash, td):
-		pattern = self.patterns[hash]
-		pattern_details = pattern.get_details(td)
-		if pattern_details["count"] > 0:
-			data_count = pattern_details["count"]
-			if data_count > 10:
-				data_count = 10
-			data = [ item for item in self.patterns[hash].latest_data if item['ts'] > td ]
-			data.reverse()
-			return {'hash': hash, 'name': pattern.name, 'hosts': pattern_details['hosts'], 'count': pattern_details['count'], 'data': data[:data_count], 'last_ts_delta': pattern.get_last_ts_delta(datetime.now())}
-		else:
-			pattern_details.update({'hash': hash})
-			return pattern_details
+    def get_digest(self, time_deltas):
+        digest = []
+        now = datetime.now()
+        for hash in self.patterns_order:
+            pattern = self.patterns[hash]
+            counts, total = pattern.get_counts(time_deltas)
+            digest.append({'name': pattern.name, 'total': total, 'counts': counts, 'hash': hash, 'is_default': (pattern.name.lower().replace(' ','') == '#default'), 'last_ts_delta': pattern.get_last_ts_delta(now)})
+        return {'digest':digest}
+
+    def get_host_data(self, hash, host, td):
+        return {'data': self.patterns[hash].get_data(host, td)}
+
+    def get_details(self, hash, td):
+        pattern = self.patterns[hash]
+        pattern_details = pattern.get_details(td)
+        if pattern_details["count"] > 0:
+            data_count = pattern_details["count"]
+            if data_count > 10:
+                data_count = 10
+            data = [ item for item in self.patterns[hash].latest_data if item['ts'] > td ]
+            data.reverse()
+            return {'hash': hash, 'name': pattern.name, 'hosts': pattern_details['hosts'], 'count': pattern_details['count'], 'data': data[:data_count], 'last_ts_delta': pattern.get_last_ts_delta(datetime.now())}
+        else:
+            pattern_details.update({'hash': hash})
+            return pattern_details
 
